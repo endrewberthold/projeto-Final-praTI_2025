09da08326f6be06a694875cad64b5e060bf7e666
Endrew Berthold <161988071+endrewberthold@users.noreply.github.com>
2025-08-12 22:15:20 -0300

    Revert "Merge da branch feature para develop"

--- enem_app/frontend/src/components/Praticar.jsx
+++ enem_app/frontend/src/components/Praticar.jsx
@@ -1,22 +1,5 @@
-// src/components/Praticar.jsx
 import React from "react";
-import LevelCard from "./LevelCard";
-import "../styles/components/levelCard.sass";
 
-
- function Praticar() {
-  return (
-    <div style={{ padding: "20px", display: "flex", justifyContent: "center" }}>
-        <LevelCard
-          titulo="lvl 001"
-          totalQuestoes={10}
-          respondidas={0}
-          textoBotao="Começar"
-          onClick={() => alert("Iniciando nível 1")} //esse botão pode ser alterado
-        />
-    </div>
-
-  );
+export default function Praticar() {
+  return <div>Praticar</div>;
 }
-
-export default Praticar;

e00f8ac265c2d0913c1455867d061a0b0cf9f820
alexia11100 <alexiasacerdotedeoliveira@gmail.com>
2025-09-19 09:24:07 -0300

    Modificação para a ultilização dos componenetes da tela login

--- enem_app/frontend/src/App.jsx
+++ enem_app/frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import Dashboard from "./components/Dashboard";
 import NavBar from "./components/NavBar";
+import TelaLogin from "./pages/TelaLogin"
 
 import "./styles/components/app.sass";
 
 function App() {
   return (
     <>
-      <h1>Aplicativo rodando :D</h1>
+      {/* <h1>Aplicativo rodando :D</h1>
       <br />
       <p>Adicione aqui seus componentes</p>
       <p>Você pode deletar esses elementos!</p>
       <NavBar />
-      <Dashboard />
+      <Dashboard /> */}
+      <TelaLogin />
       {/* <Praticar />  precisa re importar caso for usar esta pagina*/}
       {/* <FlashCards /> precisa re importar caso for usar esta pagina*/}
     </>

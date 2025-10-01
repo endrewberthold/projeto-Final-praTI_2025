1b2247a490a096760b54ac832dfcee14d6491083
Endrew Berthold <161988071+endrewberthold@users.noreply.github.com>
2025-10-01 20:19:09 -0300

    Revert "[feat] flashCardPage+flascard component + estilos"

--- enem_app/frontend/src/pages/FlashCardPage.jsx
+++ enem_app/frontend/src/pages/FlashCardPage.jsx
@@ -1,22 +1,15 @@
-import { useEffect, useState } from 'react';
-import useAuth from '../hooks/useAuth';
+import React, { useEffect, useState } from "react";
+import useAuth from "../hooks/useAuth";
 
-import FlashCard from '../components/FlashCard';
-
-import { FaBookOpen } from 'react-icons/fa';
-import { TbMathFunction } from 'react-icons/tb';
-import { GiMicroscope } from 'react-icons/gi';
-import { FaGlobeAmericas } from 'react-icons/fa';
-import { BsFillMortarboardFill } from 'react-icons/bs';
-import '../styles/pages/flashCardPage.sass';
+// import FlashCard from "../components/FlashCard";
 
 // API calls from services
 import {
   fetchFlashcardsAPI,
   newFlashcardAPI,
   deleteFlashcardAPI,
-  // updateFlashcardAPI,
-} from '../services/flashcardsServices';
+  updateFlashcardAPI,
+} from "../services/flashcardsServices";
 
 export default function FlashcardPage() {
   const { accessToken } = useAuth();
@@ -41,13 +34,13 @@ export default function FlashcardPage() {
       //console.log("FLASHCARDS DATA: ", flashcardsData);
     } catch (err) {
       if (!err?.response) {
-        setMessage('No Server Response');
+        setMessage("No Server Response");
       } else if (err.response?.status === 400) {
-        setMessage('Missing');
+        setMessage("Missing");
       } else if (err.response?.status === 401) {
-        setMessage('Unauthorized');
+        setMessage("Unauthorized");
       } else {
-        setMessage('New Flash card creation failed');
+        setMessage("New Flash card creation failed");
       }
     }
   }
@@ -57,7 +50,7 @@ export default function FlashcardPage() {
     handleFetchFlashcards();
   }, [newFlashcard]);
 
-  // FOR NEW FLASHCARD
+  // FOR NEW FLASHCARD --------------------------------------------------------------------------------------------------
   async function handleNewFlashcard(e) {
     e.preventDefault();
     console.log(term);
@@ -69,17 +62,17 @@ export default function FlashcardPage() {
         accessToken,
         term,
         areaId,
-        description,
+        description
       );
 
       setNewFlascard(response?.data);
     } catch (err) {
-      console.log('ERRO: ', err);
+      console.log("ERRO: ", err);
     }
     handleClear();
   }
 
-  // DELETE FLASHCARD
+  // DELETE FLASHCARD ----------------------------------------------------------------------------------------------
   // $ Will be refactored in the futere, it does not need a response, only if it display something in the screen
   async function handleDeleteFlashcard(item) {
     //console.log("DELETE: ", item);
@@ -88,17 +81,17 @@ export default function FlashcardPage() {
 
     try {
       const response = await deleteFlashcardAPI(accessToken, item);
-      console.log('DELETADO: ', response);
+      console.log("DELETADO: ", response);
       handleFetchFlashcards();
     } catch (err) {
-      console.log('ERRO ON DELETE CARD: ', err);
+      console.log("ERRO ON DELETE CARD: ", err);
     }
   }
 
-  // REQUEST UPDATE FLASHCARD
+  // REQUEST UPDATE FLASHCARD ------------------------------------------------------------------------------------------------
   // Will fill the input values with the select card and the id state
   async function handleRequestUpdateFlashcard(item) {
-    console.log('UPDATE: ', item);
+    console.log("UPDATE: ", item);
     //console.log("REQUEST UPDATE: ", updateFlashcard);
 
     setTerm(item.term);
@@ -107,104 +100,97 @@ export default function FlashcardPage() {
     setId(item.id);
   }
 
-  // UPDATE FLASHCARD
+  // UPDATE FLASHCARD --------------------------------------------------------------------------------------------
   // Will update the flashcard having it's id.
-  // async function handleUpdateFlashcard(e) {
-  //   e.preventDefault();
-  //   try {
-  //     const response = await updateFlashcardAPI(
-  //       accessToken,
-  //       id,
-  //       term,
-  //       areaId,
-  //       description,
-  //     );
-  //     setNewFlascard(response?.data);
-  //   } catch (err) {
-  //     console.log('ERRO: ', err);
-  //   }
-  //   handleClear();
-  // }
+  async function handleUpdateFlashcard(e) {
+    e.preventDefault();
+    try {
+      const response = await updateFlashcardAPI(
+        accessToken,
+        id,
+        term,
+        areaId,
+        description
+      );
+      setNewFlascard(response?.data);
+    } catch (err) {
+      console.log("ERRO: ", err);
+    }
+    handleClear();
+  }
 
+  // Will clear the input values
   const handleClear = () => {
-    setTerm('');
-    setDescription('');
+    setTerm("");
+    setDescription("");
   };
 
   return (
-    <section className="flashcard-container">
-      <form className="form-flashcard-container">
-        <nav className="nav-flashcard-container">
-          <h1>Criar Flashcard</h1>
-          <div className="nav-flashcard-options">
-            <label htmlFor="">Área de Conhecimento:</label>
-            <select
-              name="selectArea"
-              id="areaId"
-              onChange={(e) => setAreaId(e.target.value)}
-            >
-              <option>Selecione uma opção</option>
-              <option value="LC">Linguagens, Códigos e suas Tecnologias</option>
-              {/* These option are not working with the DB yet */}
-              {/* $ MUST IMPLEMENT - const of options and map over it*/}
-              <option value="CH">Ciências Humanas e suas Tecnologias</option>
-              <option value="CN">
-                Ciências da Natureza e suas Tecnologias
-              </option>
-              <option value="MT">Matemáticas e suas Tecnologias</option>
-            </select>
-          </div>
-        </nav>
-        <div className="user-data-flashcard-container">
-          <label>Título:</label>
+    <div>
+      <h1>FlashcardPage</h1>
+      {/* FORM FOR FLASHCARD */}
+      <div>
+        <form>
+          <label>Termo</label>
           <input
             type="text"
             onChange={(e) => setTerm(e.target.value)}
             value={term}
-            placeholder="Título"
+            placeholder="Termo"
           />
-          <label htmlFor="">Descrição:</label>
-          <textarea
-            placeholder="Dados do Flashcard"
+
+          <label htmlFor="">Area:</label>
+          <select
+            name="selectArea"
+            id="areaId"
+            onChange={(e) => setAreaId(e.target.value)}
+          >
+            <option>Selecione uma opção</option>
+            <option value="LC">Linguagens, Códigos e suas Tecnologias</option>
+            {/* These option are not working with the DB yet */}
+            {/* $ MUST IMPLEMENT - const of options and map over it*/}
+            <option value="CH">Ciências Humanas e suas Tecnologias</option>
+            <option value="CN">Ciências da Natureza e suas Tecnologias</option>
+            <option value="MT">Matemáticas e suas Tecnologias</option>
+          </select>
+
+          <label htmlFor="">Descrição</label>
+          <input
+            type="text"
+            placeholder="Descrição"
             onChange={(e) => setDescription(e.target.value)}
             value={description}
-            rows="6"
           />
-          <div className="buttons-flashcard-container">
-            <button onClick={handleNewFlashcard}>Criar Flashcard</button>
-            <button onClick={handleClear}>Limpar</button>
-          </div>
-        </div>
-      </form>
+
+          <button onClick={handleClear}>Limpar</button>
+          <button onClick={handleUpdateFlashcard}>Atualizar</button>
+          <button onClick={handleNewFlashcard}>New Flashcard</button>
+        </form>
+      </div>
+
+      {/* SECTION TO DISPLAY THE CARDS 20 PER PAGE */}
       {/* $ MUST IMPLEMENT PAGINATION - will update the 'page' api request above 'handleFetchFlashcards' and 'pages' state */}
-      <section className="icons-flashcard-container">
-        <div>
-          <BsFillMortarboardFill className="icon-flashcard" />
-          <FaBookOpen className="icon-flashcard" />
-          <TbMathFunction className="icon-flashcard" />
-          <GiMicroscope className="icon-flashcard" />
-          <FaGlobeAmericas className="icon-flashcard" />
-        </div>
-      </section>
-      <section className="flashcard-dashboard-container">
-        {flashcardsData && flashcardsData.length > 0 ? (
-          <>
-            {flashcardsData.map((item) => (
-              <FlashCard
-                key={item.id}
-                id={item.id}
-                term={item.term}
-                description={item.description}
-                area={item.areaId}
-                handleDelete={handleDeleteFlashcard}
-                handleUpdate={() => handleRequestUpdateFlashcard(item)}
-              />
-            ))}
-          </>
-        ) : (
-          <p>Loading...</p>
-        )}
-      </section>
-    </section>
+      <div>
+        <section>
+          {flashcardsData ? (
+            <div>
+              {flashcardsData.map((item) => (
+                <FlashCard
+                  key={item.id}
+                  id={item.id}
+                  term={item.term}
+                  description={item.description}
+                  area={item.areaId}
+                  handleDelete={handleDeleteFlashcard}
+                  handleUpdate={() => handleRequestUpdateFlashcard(item)}
+                />
+              ))}
+            </div>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </section>
+      </div>
+    </div>
   );
 }

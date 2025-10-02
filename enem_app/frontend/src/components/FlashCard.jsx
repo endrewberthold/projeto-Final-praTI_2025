6fa74cadb4d725ac49469824c1ac6c7252cc92f1
Andre Lucho <andretlucho@gmail.com>
2025-10-02 17:42:34 -0300

    [Fix/Refactor]Tela FlashCardPage + FlashCard

--- enem_app/frontend/src/components/FlashCard.jsx
+++ enem_app/frontend/src/components/FlashCard.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { AiFillDelete } from 'react-icons/ai';
 import { RxUpdate } from 'react-icons/rx';
 
+import { useTheme } from '../context/ThemeContext';
+
 import '../styles/components/Flashcard.sass';
 
 export default function Flashcard({
@@ -12,14 +14,16 @@ export default function Flashcard({
   handleDelete,
   handleUpdate,
 }) {
+  const { theme } = useTheme();
+
   return (
-    <div className="flashcards-dashboard">
+    <div className={`flashcards-dashboard ${theme}`}>
       <div className="title-container">
         <h3 className="flashcards-dash-title">{term}</h3>
       </div>
       <p className="flashcards-dash-description">{description}</p>
       <p className="flashcards-dash-area">{area}</p>
-      <div className="flashcards-dash-buttons">
+      <div className={`flashcards-dash-buttons ${theme}`}>
         <RxUpdate
           onClick={() => handleUpdate(id)}
           className="flashdash-icons"

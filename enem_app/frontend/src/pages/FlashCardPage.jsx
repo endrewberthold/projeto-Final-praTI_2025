a60d554e95cd10e637db6bb1512abdde38ff3e45
Andre Lucho <andretlucho@gmail.com>
2025-10-04 16:34:39 -0300

    [Refactor]Tela FlashCardPage - estilos + [Feat] ModalForm(jsx + estilo)

--- enem_app/frontend/src/pages/FlashCardPage.jsx
+++ enem_app/frontend/src/pages/FlashCardPage.jsx
@@ -3,12 +3,14 @@ import useAuth from '../hooks/useAuth';
 import { useTheme } from '../context/ThemeContext';
 
 import FlashCard from '../components/FlashCard';
+import ModalForm from '../components/ModalForm';
 
 import { FaBookOpen } from 'react-icons/fa';
 import { TbMathFunction } from 'react-icons/tb';
 import { GiMicroscope } from 'react-icons/gi';
 import { FaGlobeAmericas } from 'react-icons/fa';
 import { BsFillMortarboardFill } from 'react-icons/bs';
+
 import '../styles/pages/flashCardPage.sass';
 
 // API calls from services
@@ -31,7 +33,8 @@ export default function FlashcardPage() {
   const [areaId, setAreaId] = useState();
   const [description, setDescription] = useState();
   const [id, setId] = useState();
-  const [newFlashcard, setNewFlascard] = useState();
+  const [newFlashcard, setNewFlascard] = useState(null);
+  const [modalForm, setModalForm] = useState(true);
 
   // When the page first load it will first execute the fetch of all the user flashcards here.
   async function handleFetchFlashcards() {
@@ -67,14 +70,17 @@ export default function FlashcardPage() {
     console.log(description);
 
     try {
-      const response = await newFlashcardAPI(
-        accessToken,
-        term,
-        areaId,
-        description,
-      );
-
-      setNewFlascard(response?.data);
+      if (term != 'Selecione uma opção' && areaId && description) {
+        const response = await newFlashcardAPI(
+          accessToken,
+          term,
+          areaId,
+          description,
+        );
+        setNewFlascard(response?.data);
+      } else {
+        setModalForm((prev) => !prev);
+      }
     } catch (err) {
       console.log('ERRO: ', err);
     }
@@ -128,82 +134,97 @@ export default function FlashcardPage() {
     handleClear();
   }
 
+  // useEffect(() => console.log(dataForm), [dataForm]);
+
   const handleClear = () => {
     setTerm('');
     setDescription('');
   };
 
+  const handleCloseModal = () => {
+    setModalForm(false);
+  };
+
   return (
-    <section className="flashcard-container">
-      <form className="form-flashcard-container">
-        <nav className="nav-flashcard-container">
+    <>
+      {modalForm && <ModalForm onClose={handleCloseModal} />}
+      <section className="flashcard-container">
+        <form
+          className={`form-flashcard-container ${modalForm ? 'modal' : ''}`}
+        >
           <h1>Criar Flashcard</h1>
-          <div className="nav-flashcard-options">
-            <label htmlFor="">Área de Conhecimento:</label>
-            <select
-              name="selectArea"
-              id="areaId"
-              onChange={(e) => setAreaId(e.target.value)}
-            >
-              <option>Selecione uma opção</option>
-              <option value="LC">Linguagens, Códigos e suas Tecnologias</option>
-              <option value="CH">Ciências Humanas e suas Tecnologias</option>
-              <option value="CN">
-                Ciências da Natureza e suas Tecnologias
-              </option>
-              <option value="MT">Matemáticas e suas Tecnologias</option>
-            </select>
-          </div>
-        </nav>
-        <div className="user-data-flashcard-container">
-          <label>Título:</label>
-          <input
-            type="text"
-            onChange={(e) => setTerm(e.target.value)}
-            value={term}
-            placeholder="Título"
-          />
-          <label htmlFor="">Descrição:</label>
-          <textarea
-            placeholder="Dados do Flashcard"
-            onChange={(e) => setDescription(e.target.value)}
-            value={description}
-            rows="6"
-          />
-          <div className="buttons-flashcard-container">
-            <button onClick={handleNewFlashcard}>Criar Flashcard</button>
-            <button onClick={handleClear}>Limpar</button>
-          </div>
-        </div>
-      </form>
-      <section className="icons-flashcard-container">
-        <div>
-          <BsFillMortarboardFill className={`icon-flashcard ${theme}`} />
-          <FaBookOpen className={`icon-flashcard ${theme}`} />
-          <TbMathFunction className={`icon-flashcard ${theme}`} />
-          <GiMicroscope className={`icon-flashcard ${theme}`} />
-          <FaGlobeAmericas className={`icon-flashcard ${theme}`} />
-        </div>
-      </section>
-      <section className={`flashcard-dashboard-container ${theme}`}>
-        {flashcardsData && flashcardsData.length > 0 ? (
-          <>
-            {flashcardsData.map((item) => (
-              <FlashCard
-                key={item.id}
-                id={item.id}
-                term={item.term}
-                description={item.description}
-                area={item.areaId}
-                handleDelete={handleDeleteFlashcard}
-                handleUpdate={() => handleRequestUpdateFlashcard(item)}
+          <nav className="nav-flashcard-container">
+            <div className="nav-flashcard-title">
+              <label>Título:</label>
+              <input
+                type="text"
+                onChange={(e) => setTerm(e.target.value)}
+                value={term}
+                placeholder="Título"
               />
-            ))}
-          </>
-        ) : (
-          <p>Loading...</p>
-        )}
+            </div>
+            <div className="nav-flashcard-options">
+              <label htmlFor="">Área de Conhecimento:</label>
+              <select
+                name="selectArea"
+                id="areaId"
+                onChange={(e) => setAreaId(e.target.value)}
+              >
+                <option>Selecione uma opção</option>
+                <option value="LC">
+                  Linguagens, Códigos e suas Tecnologias
+                </option>
+                <option value="CH">Ciências Humanas e suas Tecnologias</option>
+                <option value="CN">
+                  Ciências da Natureza e suas Tecnologias
+                </option>
+                <option value="MT">Matemáticas e suas Tecnologias</option>
+              </select>
+            </div>
+          </nav>
+          <div className="description-flashcard-container">
+            <label htmlFor="">Descrição:</label>
+            <textarea
+              placeholder="Dados do Flashcard"
+              onChange={(e) => setDescription(e.target.value)}
+              value={description}
+              rows="6"
+            />
+            <div className="buttons-flashcard-container">
+              <button onClick={handleNewFlashcard}>Criar Flashcard</button>
+              <button onClick={handleClear}>Limpar</button>
+            </div>
+          </div>
+        </form>
+        <section className="icons-flashcard-container">
+          <div>
+            <BsFillMortarboardFill className={`icon-flashcard ${theme}`} />
+            <FaBookOpen className={`icon-flashcard ${theme}`} />
+            <TbMathFunction className={`icon-flashcard ${theme}`} />
+            <GiMicroscope className={`icon-flashcard ${theme}`} />
+            <FaGlobeAmericas className={`icon-flashcard ${theme}`} />
+          </div>
+        </section>
+        <section className={`flashcard-dashboard-container ${theme}`}>
+          {flashcardsData && flashcardsData.length > 0 ? (
+            <>
+              {flashcardsData.map((item) => (
+                <FlashCard
+                  key={item.id}
+                  id={item.id}
+                  term={item.term}
+                  description={item.description}
+                  area={item.areaId}
+                  handleDelete={handleDeleteFlashcard}
+                  handleUpdate={() => handleRequestUpdateFlashcard(item)}
+                />
+              ))}
+            </>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </section>
       </section>
-    </section>
+    </>
   );
 }

53112ed8dee65d09860f51d2a0a4594f58a0ce88
Andre Lucho <andretlucho@gmail.com>
2025-10-07 17:05:13 -0300

    [feat]roter to viewFlashCard

--- enem_app/frontend/src/components/FlashCard.jsx
+++ enem_app/frontend/src/components/FlashCard.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { AiFillDelete } from 'react-icons/ai';
-import { RxUpdate } from 'react-icons/rx';
 
 import { useTheme } from '../context/ThemeContext';
+import { useNavigate } from 'react-router-dom';
+
+import { AiFillDelete } from 'react-icons/ai';
+import { RxUpdate } from 'react-icons/rx';
 
 import '../styles/components/Flashcard.sass';
 
@@ -15,6 +17,13 @@ export default function Flashcard({
   handleUpdate,
 }) {
   const { theme } = useTheme();
+
+  const navigate = useNavigate();
+
+  const handleNavigate = (e, term, description, area) => {
+    e.preventDefault();
+    navigate(`/viewFlashPage/${id}`, { state: { term, description, area } });
+  };
 
   return (
     <div className={`flashcards-dashboard ${theme}`}>
@@ -34,6 +43,32 @@ export default function Flashcard({
           className="flashdash-icons"
         />
       </div>
+      <button onClick={(e) => handleNavigate(e, term, description, area)}>
+        abrir
+      </button>
     </div>
   );
 }
+
+// return (
+//   <NavLink to={`/viewFlashPage/${idd}`}>
+//     <div className={`flashcards-dashboard ${theme}`}>
+//       <p className="flashcards-dash-area">{area}</p>
+//       <div className="flashcard-inner-container">
+//         <div className="title-container">
+//           <h3 className="flashcards-dash-title">{term}</h3>
+//         </div>
+//       </div>
+//       <div className={`flashcards-dash-buttons ${theme}`}>
+//         <RxUpdate
+//           onClick={() => handleUpdate(id)}
+//           className="flashdash-icons"
+//         />
+//         <AiFillDelete
+//           onClick={() => handleDelete(id)}
+//           className="flashdash-icons"
+//         />
+//       </div>
+//     </div>
+//   </NavLink>
+// );

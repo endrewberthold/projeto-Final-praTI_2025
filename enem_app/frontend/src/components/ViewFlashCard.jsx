7d5ec9d8f03ae6ba1322555256bd9c42ac09b9c9
Andre Lucho <andretlucho@gmail.com>
2025-10-08 16:19:31 -0300

    [feat]start ViewFlashCard style

--- enem_app/frontend/src/components/ViewFlashCard.jsx
+++ enem_app/frontend/src/components/ViewFlashCard.jsx
@@ -1,151 +1,74 @@
-import React, { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 
-// import { LuFileQuestion } from 'react-icons/lu';
-// import { TiPencil } from 'react-icons/ti';
-// import { FaRegTrashAlt } from 'react-icons/fa';
+import { LuFileQuestion } from 'react-icons/lu';
+import { TiPencil } from 'react-icons/ti';
+import { FaRegTrashAlt } from 'react-icons/fa';
 
-// import '../styles/components/ViewFlashCard.sass';
+import '../styles/components/ViewFlashCard.sass';
 
-const ViewFlashCard = () => {
+const ViewFlashCard = ({ onClose }) => {
   const params = useParams();
   const location = useLocation();
   const { term, description, areaId } = location.state || {};
 
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [cardContent, setCardContent] = useState(false);
+
+  // inicialização do cartão com um conteúdo do back ou vazio
   useEffect(() => {
-    console.log('ID do Flashcard (via URL):', params.id);
-    console.log('Dados do Flashcard (via state):', {
-      term,
-      description,
-      areaId,
-    });
-  }, [params.id, term, description, areaId]);
+    if (term && description && areaId) {
+      setCardContent(true);
+    }
+  }, [term, description, areaId]);
+
+  //Editar ?
+  const handleEdit = () => {
+    console.log('Editar card');
+  };
+
+  //Deletar ?
+  const handleDelete = () => {
+    if (window.confirm('Tem certeza que deseja excluir este card?')) {
+      // Logic to handle delete functionality
+      console.log(`Card ${term} excluído!`);
+      onClose();
+    }
+  };
+
+  // verificação e trigger do flip
+  const handleCardClick = (e) => {
+    if (
+      e.target.tagName !== 'BUTTON' &&
+      e.target.tagName !== 'TEXTAREA' &&
+      e.target.className !== 'card-actions-container'
+    ) {
+      handleFlip();
+    }
+  };
 
   return (
-    <div>
-      ViewFlashCard
-      <p>ID do card: {params.id}</p>
-      <p>Habilidade: {areaId}</p>
-      <p>Anotações user:{description}</p>
-    </div>
+    <section className="card-container">
+      <div
+        className={`card ${isFlipped ? 'flipped' : ''}`}
+        onClick={() => setIsFlipped((prev) => !prev)}
+      >
+        <div>{params.id}</div>
+        <div className="innerCard">
+          <div className="face frontside">
+            <span>{areaId}</span>
+            <span className="question-text">{term}</span>
+          </div>
+          <div className="face backside">
+            <span>{areaId}</span>
+            <span className="answer-text">{description}</span>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
 export default ViewFlashCard;
-// const Flashcard = ({ onClose }) => {
-//   const [isFlipped, setIsFlipped] = useState(false);
-//   const [cardContent, setCardContent] = useState('');
 
-//   const { data } = useParams();
-
-//   // const content = {
-//   //   initialContent = '',
-//   // }
-
-//   useEffect(() => {
-//     console.log(data);
-//   }, [data]);
-
-//   // inicialização do cartão com um conteúdo do back ou vazio
-//   // useEffect(() => {
-//   //   if (initialContent) {
-//   //     setCardContent(initialContent);
-//   //   }
-//   // }, [initialContent]);
-
-//   // capturando valores da textarea
-//   const handleTextareaChange = (event) => {
-//     setCardContent(event.target.value);
-//   };
-
-//   // trocando valores de 'isFlipped' p iniciar a ação de flip(virar)
-//   const handleFlip = () => {
-//     setIsFlipped((prev) => !prev);
-//   };
-
-//   //futura ação de editar
-//   const handleEdit = () => {
-//     console.log('Editar card');
-//   };
-
-//   // deletar cartão
-//   const handleDelete = () => {
-//     if (window.confirm('Tem certeza que deseja excluir este card?')) {
-//       // Logic to handle delete functionality
-//       console.log(`Card ${title} excluído!`);
-//       onClose();
-//     }
-//   };
-
-//   // verificação e trigger do flip
-//   const handleCardClick = (e) => {
-//     e.stopPropagation();
-//     if (
-//       e.target.tagName !== 'BUTTON' &&
-//       e.target.tagName !== 'TEXTAREA' &&
-//       e.target.className !== 'card-actions-container'
-//     ) {
-//       handleFlip();
-//     }
-//   };
-
-//   return (
-//     <div className="flashcard-modal-overlay" onClick={onClose}>
-//       <div
-//         className={`flashcard-container ${isFlipped ? 'is-flipped' : ''}`}
-//         onClick={handleCardClick}
-//       >
-//         {/* Frente do Card */}
-//         <div className="flashcard-front-container">
-//           {difficulty && (
-//             <div
-//               className={`difficulty-indicator difficulty-${difficulty.toLowerCase()}`}
-//             ></div>
-//           )}
-//           <h2 className="card-title">{title}</h2>
-//           <button
-//             className={`remember-button difficulty-${difficulty.toLowerCase()}`}
-//           >
-//             Lembrar-se
-//           </button>
-//         </div>
-
-//         {/* Verso do Card */}
-//         <div className="flashcard-back-container">
-//           {difficulty && (
-//             <div
-//               className={`difficulty-indicator difficulty-${difficulty.toLowerCase()}`}
-//             ></div>
-//           )}
-//           <div className="card-back-title-container">
-//             <h2 className="card-title card-back-tilte">{title}</h2>
-//             <LuFileQuestion className="luFile-icon" />
-//           </div>
-//           <textarea
-//             className={`card-textarea difficulty-${difficulty.toLowerCase()}`}
-//             value={cardContent}
-//             onChange={handleTextareaChange}
-//             placeholder="Explicação da palavra/Anotações aqui (deve ter um máximo de caracteres)"
-//           ></textarea>
-//           <div className="card-actions-container">
-//             <button
-//               className={`ocultar-btn difficulty-${difficulty.toLowerCase()}`}
-//               onClick={handleFlip}
-//             >
-//               Ocultar
-//             </button>
-//             <div className="action-btn">
-//               <TiPencil className="action-btn-icon" onClick={handleEdit} />
-//               <FaRegTrashAlt
-//                 className="action-btn-icon"
-//                 onClick={handleDelete}
-//               />
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Flashcard;
+// e.stopPropagation();

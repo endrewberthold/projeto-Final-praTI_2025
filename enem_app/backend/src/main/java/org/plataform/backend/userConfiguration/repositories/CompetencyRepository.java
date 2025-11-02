60b41313732468cbf2229ab248cf99a99de887a3
Endrew Berthold <endrew.berthold@gmail.com>
2025-11-02 01:23:40 -0300

    [ADD] Acrescentada novas saidas de dados no endpoint /start na resposta das questões, precisa ser ajustado a implementação

--- enem_app/backend/src/main/java/org/plataform/backend/userConfiguration/repositories/CompetencyRepository.java
+++ enem_app/backend/src/main/java/org/plataform/backend/userConfiguration/repositories/CompetencyRepository.java
@@ -4,6 +4,9 @@ import org.plataform.backend.userConfiguration.entity.Competency;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CompetencyRepository extends JpaRepository<Competency, Long> {
+    List<Competency> findAllByIdIn(List<Long> ids);
 }
